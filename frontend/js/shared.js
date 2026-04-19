65f57cf13b78b91a86a8896f35f5b18f5e757885
S Prajyot Vijay <prajyot.v.surwade@gmail.com>
2026-04-19 19:10:36 +0530

    feat: update API_BASE to production Render URL

--- frontend/js/shared.js
+++ frontend/js/shared.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://lifeline-backend-sm5o.onrender.com/api';
 
 function renderNavbar(activePage = '', root = '../') {
   return `
